90ec9bca63e7631b18e08b91c09a5b4fa0c462d4
Gourav Tiwari <gouravtiwari21@gmail.com>
2011-01-25 21:57:04 -0500

    initial commit with rails 3.0.3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tiniblog::Application.config.secret_token = 'd7b1eb800509cfa3484e72580bb8c5b1f69ad8326a551afe41d35b334233b01944039b4eb8f79f9871f53fc174c7a38d8cab1f6bbc2c0797a54c96521ab67f78'
